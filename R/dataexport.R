9331b8222bce77e76765354741193fa094884c27
EvaMaeRey <evangeline.mae@gmail.com>
2022-03-15 19:00:11 -0400

    initial commit tidytitanic

---
+++ R/dataexport.R
@@ -0,0 +1,26 @@
+#' Records for individuals on titanic.
+#'
+#' A dataset ... blah blah
+#'
+#' @format A data frame with 2201 rows and 5 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source R stat
+"tidy_titanic"
+
+
+#' Summaries of people and outcomes on titanic
+#'
+#' A dataset containing blahhhhhhh
+#'
+#' @format A data frame with 32 rows and 5 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"flat_titanic"
