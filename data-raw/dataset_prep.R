5f0e199210183db5224214c9d6a343434189f093
EvaMaeRey <evangeline.mae@gmail.com>
2022-10-31 10:23:16 -0400

    adjusting steps hierachy

--- data-raw/dataset_prep.R
+++ data-raw/dataset_prep.R
@@ -2,15 +2,18 @@ library(tidyverse)
 
 Titanic %>%
   data.frame() %>%
-  janitor::clean_names() ->
+  janitor::clean_names() %>%
+  tibble() ->
   flat_titanic
 
 Titanic %>%
   data.frame() %>%
   janitor::clean_names()%>%
   uncount(weights = freq) %>%
-  mutate(id = 1:n(), .before = 1) ->
+  mutate(id = 1:n(), .before = 1) %>%
+  tibble::remove_rownames() %>%
+  tibble() ->
   tidy_titanic
 
-usethis::use_data(tidy_titanic, overwrite = TRUE)
 usethis::use_data(flat_titanic, overwrite = TRUE)
+usethis::use_data(tidy_titanic, overwrite = TRUE)
