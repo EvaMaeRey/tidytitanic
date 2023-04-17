a22673785e7ae645c2b0ac718f1cdd2c731e8a34
EvaMaeRey <evangeline.mae@gmail.com>
2023-04-17 17:26:00 -0400

    adding passenger level data from Claus' book data suppliment

--- R/dataexport.R
+++ R/dataexport.R
@@ -31,3 +31,19 @@
 #' }
 #' @source \url{http://www.diamondse.info/}
 "flat_titanic"
+
+#' Records for individual passengers on titanic; names recorded.
+#'
+#' A dataset ... blah blah
+#'
+#' @format A data frame with 1313 rows and 5 variables:
+#' \describe{
+#'   \item{name}{weight of the diamond, in carats}
+#'   \item{class}{weight of the diamond, in carats}
+#'   \item{age}{price, in US dollars}
+#'   \item{sex}{weight of the diamond, in carats}
+#'   \item{survived}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source R stat
+"passengers"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -12,5 +12,15 @@ Titanic %>%
   mutate(id = 1:n(), .before = 1) ->
   tidy_titanic
 
+tidy_titanic %>% filter(class != "Crew")
+
 usethis::use_data(tidy_titanic, overwrite = TRUE)
 usethis::use_data(flat_titanic, overwrite = TRUE)
+
+# added https://github.com/clauswilke/dviz.supp/blob/master/data/titanic_all.rda as well
+# added passenger_detail
+
+passengers <- titanic_all
+save(passengers, file = "data/passengers.rda")
+usethis::use_data(passengers, overwrite = TRUE)
+
